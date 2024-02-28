63a4035e3c6e88c2d5ef71dfe86896aa050f485c
mattKimmmm <mkim25@wustl.edu>
2024-02-28 17:39:26 -0600

    pp

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,63 @@
 import pandas as pd
 import os
 
-from utils import read_csv, get_icd_codes
+from utils import read_csv, get_icd_codes, save_to_csv_multiple
 
-df_icu_stays = read_csv('ICUSTAYS.csv')
-
-# aggregate patient for a given keyword
-def patients_aggregate(keyword):
+# aggregate patient/event tables for a given keyword (deterioration)
+def patients_events(keyword):
     # get icd codes for the given keyword
     icd_codes = get_icd_codes(keyword)
 
     if icd_codes is not None:
-        # get all patients with cardiac arrest
-        patients = df_icu_stays['subject_id'][df_icu_stays['hadm_id'].isin(icd_codes)]
-        print(f"Patients with cardiac arrest: {patients.head(5)}")
-        return patients
+        # get subject_id and hadm_id from DIAGNOSES_ICD where icd9_code is in icd_codes
+        df_diagnoses_icd = read_csv('DIAGNOSES_ICD.csv')
+        df_diagnoses_icd = df_diagnoses_icd[df_diagnoses_icd['icd9_code'].isin(icd_codes)]
+        # print(df_diagnoses_icd.head(5))
+
+        # get patients from PATIENTS where subject_id is in df_diagnoses_icd
+        df_patients = read_csv('PATIENTS.csv')
+        df_patients = df_patients[df_patients['subject_id'].isin(df_diagnoses_icd['subject_id'])]
+        # print(df_patients.head(5))
+        # print(f"Number of patients with {keyword}: {df_patients.shape[0]}")
+
+        # get icu stays from ICUSTAYS where subject_id is in df_patients
+        df_icustays = read_csv('ICUSTAYS.csv')
+        df_icustays = df_icustays[df_icustays['subject_id'].isin(df_patients['subject_id'])]
+        # only stays < 2 days
+        # df_icustays = df_icustays[df_icustays['los'] <= 2]
+        print(df_icustays.head(5))
+        print(f"Number of ICU stays with {keyword}: {df_icustays.shape[0]}")
+
+        # df_icustays_hadm = df_icustays[df_icustays['hadm_id'].isin(df_diagnoses_icd['hadm_id'])]
+        # print(df_icustays_hadm.head(5))
+        # print(f"(using hadm)Number of ICU stays with {keyword}: {df_icustays_hadm.shape[0]}")
+        
+        # get lab results for selected patients' icu stays
+        df_chart_events= read_csv('CHARTEVENTS.csv')
+        df_chart_events = df_chart_events[df_chart_events['icustay_id'].isin(df_icustays['icustay_id'])]
+        # print(df_chart_events.head(5))
+        # print(f"Number of lab results with {keyword}: {df_chart_events.shape[0]}\n")
+
+        df_output_events = read_csv('OUTPUTEVENTS.csv')
+        df_output_events = df_output_events[df_output_events['icustay_id'].isin(df_icustays['icustay_id'])]
+        # print(df_output_events.head(5))
+        # print(f"Number of output events with {keyword}: {df_output_events.shape[0]}\n")
+
+        df_lab_events = read_csv('LABEVENTS.csv')
+        df_lab_icu_joined = pd.merge(df_lab_events, df_icustays, on=['subject_id', 'hadm_id'])
+        # print(df_lab_icu_joined.head(5))
+        # print(f"Number of lab events in ICU with {keyword}: {df_lab_icu_joined.shape[0]}\n")
+
+        df_lab_icu_specific = df_lab_icu_joined[
+        (df_lab_icu_joined['charttime'] >= df_lab_icu_joined['intime']) &
+        (df_lab_icu_joined['charttime'] <= df_lab_icu_joined['outtime'])
+        ]
+        # print(df_lab_icu_specific.head(5))
+        # print(f"Number of lab events in ICU with {keyword}: {df_lab_icu_specific.shape[0]}\n")
+
+        # save df to csv
+        # save_to_csv_multiple([df_patients, df_chart_events, df_output_events, df_lab_icu_specific],
+        #                      [f'patients_{keyword}', f'chart_events_{keyword}',
+        #                       f'output_events_{keyword}', f'lab_icu_specific_{keyword}'])
+
+        return (df_patients, df_chart_events, df_output_events, df_lab_icu_specific)
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 import os
+
 
 # read csv file
 def read_csv(file_name):
@@ -8,7 +11,7 @@ def read_csv(file_name):
 
     try:
         df = pd.read_csv(file_path)
-        print(df.head(5))
+        # print(df.head(5))
         return df
     except FileNotFoundError:
         print(f"File {file_path} not found")
@@ -27,8 +30,35 @@ def get_icd_codes(keyword, file='D_ICD_DIAGNOSES.csv'):
     # get the value of icd9_code column where either of short_title and long_title column contains the keyword
     icd_codes = df['icd9_code'][(df['short_title'].str.contains(keyword, case=False)) | 
                                 (df['long_title'].str.contains(keyword, case=False))]
-    print(f"ICD codes for {keyword}:")
-    # row_id, icd_code
-    print(icd_codes.head(5))
+    
+    # row_id, icd_code (77895 for newborn cardiac arrest, we might want to exclude this)
+    # print(f"ICD codes for {keyword}:")
+    # print(icd_codes.head(5))
 
     return icd_codes
+
+# get analytics for a given column (for numeric columns)
+def column_analytics(file_name, column_name, num_bins):
+    df = read_csv(file_name)
+    if df is not None:
+        print(f"Mean: {df[column_name].mean()}")
+        print(f"Median: {df[column_name].median()}")
+        print(f"Standard Deviation: {df[column_name].std()}")
+        print(f"Minimum: {df[column_name].min()}")
+        print(f"Maximum: {df[column_name].max()}")
+        
+        # 10-bin histogram
+        plt.hist(df[column_name], bins=num_bins)
+        plt.show()
+
+# save single pd dataframe to csv
+def save_to_csv(df, file_name):
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, "data", "processed", file_name)
+    df.to_csv(file_path, index=False)
+    print(f"Dataframe saved to {file_path}")
+
+# save multiple pd dataframes to csv
+def save_to_csv_multiple(dfs, file_names):
+    for i in range(len(dfs)):
+        save_to_csv(dfs[i], file_names[i])
