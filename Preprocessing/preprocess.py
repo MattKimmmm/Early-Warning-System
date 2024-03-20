2d7bb89ec6c5709be99bcfd3da593c90e503352b
yurock23 <112423753+Paul-Heo@users.noreply.github.com>
2024-03-20 17:37:57 -0500

    파일 정리

---
+++ Preprocessing/preprocess.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import os
+import numpy as np
+
+from utils import read_csv, get_icd_codes, save_to_csv_multiple, load_from_csv_multiple, unique_column_df, column_analytics_df, load_from_csv
+from utils import split_by_patients, aggregate_events_for_item, aggregate_events_by_time, column_average, save_to_npy
+
+# aggregate patient/event tables for a given keyword (deterioration)
+def patients_events(keyword):
+    # get icd codes for the given keyword
+    icd_codes = get_icd_codes(keyword)
+
+    if icd_codes is not None:
+        # get subject_id and hadm_id from DIAGNOSES_ICD where icd9_code is in icd_codes
+        df_diagnoses_icd = read_csv('DIAGNOSES_ICD.csv')
+        df_diagnoses_icd = df_diagnoses_icd[df_diagnoses_icd['ICD9_CODE'].isin(icd_codes)]
+        # print(df_diagnoses_icd.head(5))
+
+        # get patients from PATIENTS where subject_id is in df_diagnoses_icd
+        df_patients = read_csv('PATIENTS.csv')
+        df_patients = df_patients[df_patients['SUBJECT_ID'].isin(df_diagnoses_icd['SUBJECT_ID'])]
+        print(df_patients.head(5))
+        print(f"Number of patients with {keyword}: {df_patients.shape[0]}")
+
+        # get icu stays from ICUSTAYS where subject_id is in df_patients
+        df_icustays = read_csv('ICUSTAYS.csv')
+        df_icustays_subset = df_icustays[['ICUSTAY_ID', 'INTIME', 'OUTTIME']]
+        df_icustays = df_icustays[df_icustays['SUBJECT_ID'].isin(df_patients['SUBJECT_ID'])]
+        # only stays < 3 days
+        # df_icustays = df_icustays[df_icustays['LOS'] <= 3]
+        print(df_icustays.head(5))
+        print(f"Number of ICU stays with {keyword}: {df_icustays.shape[0]}")
+
+        # df_icustays_hadm = df_icustays[df_icustays['hadm_id'].isin(df_diagnoses_icd['hadm_id'])]
+        # print(df_icustays_hadm.head(5))
+        # print(f"(using hadm)Number of ICU stays with {keyword}: {df_icustays_hadm.shape[0]}")
+        
+        # get lab results for selected patients' icu stays
+        columns_to_use = ["ROW_ID", "SUBJECT_ID", "HADM_ID", "ITEMID", "CHARTTIME", "STORETIME", "CGID", "VALUENUM", "VALUEUOM", "WARNING", "ERROR"]
+        df_chart_events= read_csv('CHARTEVENTS.csv', columns_to_use=columns_to_use)
+        # for demo
+        # df_chart_events= read_csv('CHARTEVENTS.csv')
+        df_chart_events = pd.merge(df_chart_events, df_icustays_subset, on='ICUSTAY_ID', how='inner')
+        print(df_chart_events.head(5))
+        print(f"Number of chart events with {keyword}: {df_chart_events.shape[0]}\n")
+
+        columns_to_use = ["ROW_ID", "SUBJECT_ID", "HADM_ID", "CHARTTIME", "ITEMID", "VALUENUM", "VALUEUOM", "FLAG"]
+        df_output_events = read_csv('OUTPUTEVENTS.csv', columns_to_use=columns_to_use)
+        # for demo
+        # df_output_events = read_csv('OUTPUTEVENTS.csv')
+        df_output_events = pd.merge(df_output_events, df_icustays_subset, on='ICUSTAY_ID', how='inner')
+        print(df_output_events.head(5))
+        print(f"Number of output events with {keyword}: {df_output_events.shape[0]}\n")
+
+        columns_to_use = ["ROW_ID", "SUBJECT_ID", "HADM_ID", "ICUSTAY_ID", "CHARTTIME", "ITEMID", "VALUE", "VALUEUOM", "CGID"]
+        df_lab_events = read_csv('LABEVENTS.csv', columns_to_use=columns_to_use)
+        # for demo
+        # df_lab_events = read_csv('LABEVENTS.csv')
+        df_lab_icu_joined = pd.merge(df_lab_events, df_icustays, on=['SUBJECT_ID', 'HADM_ID'])
+
+        df_lab_icu_specific = df_lab_icu_joined[
+        (df_lab_icu_joined['CHARTTIME'] >= df_lab_icu_joined['INTIME']) &
+        (df_lab_icu_joined['CHARTTIME'] <= df_lab_icu_joined['OUTTIME'])
+        ]
+        print(df_lab_icu_specific.head(5))
+        print(f"Number of lab events in ICU with {keyword}: {df_lab_icu_specific.shape[0]}\n")
+
+        # read dictionaries
+        columns_to_use = ["ROW_ID", "ITEMID", "DBSOURCE", "LINKSTO"]
+        df_d_items = read_csv('D_ITEMS.csv', columns_to_use=columns_to_use)
+        # for demo
+        # df_d_items = read_csv('D_ITEMS.csv')
+        print(df_d_items.head(5))
+        print(f"Number of items: {df_d_items.shape[0]}\n")
+
+        columns_to_use = ["ROW_ID", "ITEMID", "LABEL", "FLUID", "CATEGORY", "LOINC_CODE"]
+        df_d_labitems = read_csv('D_LABITEMS.csv', columns_to_use=columns_to_use)
+        # for demo
+        # df_d_labitems = read_csv('D_LABITEMS.csv')
+        print(df_d_labitems.head(5))
+        print(f"Number of lab items: {df_d_labitems.shape[0]}\n")
+
+        # join events with dictionaries for item description
+        df_chart_events = pd.merge(df_chart_events, df_d_items, on='ITEMID')
+        df_output_events = pd.merge(df_output_events, df_d_items, on='ITEMID')
+        df_lab_icu_specific = pd.merge(df_lab_icu_specific, df_d_labitems, on='ITEMID')
+
+        #
+
+
+        # save df to csv
+        save_to_csv_multiple([df_patients, df_chart_events, df_output_events, df_lab_icu_specific],
+                             [f'patients_{keyword}', f'chart_events_{keyword}',
+                              f'output_events_{keyword}', f'lab_icu_specific_{keyword}'])
+
+        return (df_patients, df_chart_events, df_output_events, df_lab_icu_specific)
+
+
+    
+
+# aggregate events into hourly bins
+def aggregate_events(keyword):
+    # load dataframes
+    df_patients = load_from_csv(f'patients_{keyword}')
+    [df_chart_event, df_output_event, df_lab_icu_specific] = load_from_csv_multiple([f'chart_events_{keyword}', f'output_events_{keyword}', f'lab_icu_specific_{keyword}'])
+
+    print(f"Unique items in CHARTEVENTS with {keyword}:")
+    df_chart_unique = unique_column_df(df_chart_event, 'ITEMID')
+
+    print(f"Unique items in OUTPUTEVENTS with {keyword}:")
+    df_output_unique = unique_column_df(df_output_event, 'ITEMID')
+
+    print(f"Unique items in LABEVENTS with {keyword}:")
+    df_lab_unique = unique_column_df(df_lab_icu_specific, 'ITEMID')
+
+    # join patients with events
+    df_chart_patient = pd.merge(df_patients, df_chart_event, on='SUBJECT_ID', how='inner')
+    print(f"Number of unique patients with {keyword}: {df_chart_patient['SUBJECT_ID'].nunique()}")
+    print(f"Number of chart events with {keyword}: {df_chart_patient.shape[0]}")
+    print(df_chart_patient.head(5))
+    df_output_patient = pd.merge(df_patients, df_output_event, on='SUBJECT_ID', how='inner')
+    print(f"Number of unique patients with {keyword}: {df_output_patient['SUBJECT_ID'].nunique()}")
+    print(f"Number of output events with {keyword}: {df_output_patient.shape[0]}")
+    print(df_output_patient.head(5))
+    df_lab_patient = pd.merge(df_patients, df_lab_icu_specific, on='SUBJECT_ID', how='inner')
+    print(f"Number of unique patients with {keyword}: {df_lab_patient['SUBJECT_ID'].nunique()}")
+    print(f"Number of lab events with {keyword}: {df_lab_patient.shape[0]}")
+    print(df_lab_patient.head(5))
+
+    # aggregate events by subjects
+    # output events
+    # input shape: [# patients, # hours (72), # unique items]
+    input = []
+    for patient in split_by_patients(df_output_patient):
+        patient_specific = []
+        # print(f"Number of output events for patient {patient['SUBJECT_ID'].iloc[0]}: {patient.shape[0]}")
+        print(patient.head(5))
+
+        intime = patient['INTIME'].iloc[0]
+
+        # items_dfs = aggregate_events_for_item(patient, df_output_unique)
+
+        times_dfs = aggregate_events_by_time(patient, intime)
+
+        # for each hour, aggregate value by item and make a list
+        for i, times_df in enumerate(times_dfs):
+            items_aggregated = []
+
+            if times_df.empty:
+                items_aggregated.append([0] * len(df_output_unique))
+            else:
+                items_dfs = aggregate_events_for_item(times_df, df_output_unique)
+                unique_item_aggregated = []
+                for item_df in items_dfs:
+                    if item_df.empty:
+                        unique_item_aggregated.append(0)
+                    else:
+                        unique_item_aggregated.append(column_average(item_df, 'VALUE'))
+
+                items_aggregated.append(unique_item_aggregated)
+                # print(unique_item_aggregated)
+                # print(f"length: {len(unique_item_aggregated)}")
+            
+            patient_specific.append(items_aggregated)
+            
+        input.append(patient_specific)
+    
+    # print input shape
+    num_patients = len(input)
+    num_hours = len(input[0])
+    num_items = len(input[0][0][0])
+    # print(input[0][0])
+    print(f"Input dimensions: {num_patients} patients x {num_hours} hours x {num_items} items")
+    # print(input)
+    save_to_npy(input, f'input_{keyword}')
+
+
+    
---
+++ Preprocessing/utils.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+
+
+# read csv file
+def read_csv(file_name, columns_to_use=None):
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, "data", file_name)
+
+    try:
+        if columns_to_use is not None:
+            df = pd.read_csv(file_path, usecols=columns_to_use, low_memory=False)
+        else:
+            df = pd.read_csv(file_path, low_memory=False)
+        
+        df.columns = df.columns.str.upper()
+        # print(df.head(5))
+        return df
+    except FileNotFoundError:
+        print(f"File {file_path} not found")
+        return None
+    
+# print unique column values given file name and column name
+def unique_column_values(file_name, column_name):
+    df = read_csv(file_name)
+    if df is not None:
+        print(df[column_name].unique())
+
+# print unique column values given df and column name
+def unique_column_df(df, column_name):
+    print(df[column_name].unique())
+    print(f"Number of unique values: {len(df[column_name].unique())}")
+    return df[column_name].unique()
+
+# get icd codes given keyword
+def get_icd_codes(keyword, file='D_ICD_DIAGNOSES.csv'):
+    df = read_csv(file)
+    
+    # get the value of icd9_code column where either of short_title and long_title column contains the keyword
+    icd_codes = df['ICD9_CODE'][(df['SHORT_TITLE'].str.contains(keyword, case=False)) | 
+                                (df['LONG_TITLE'].str.contains(keyword, case=False))]
+    
+    # row_id, icd_code (77895 for newborn cardiac arrest, we might want to exclude this)
+    # print(f"ICD codes for {keyword}:")
+    # print(icd_codes.head(5))
+
+    return icd_codes
+
+# get analytics for a given column (for numeric columns)
+def column_analytics(file_name, column_name, num_bins):
+    df = read_csv(file_name)
+    if df is not None:
+        print(f"Mean: {df[column_name].mean()}")
+        print(f"Median: {df[column_name].median()}")
+        print(f"Standard Deviation: {df[column_name].std()}")
+        print(f"Minimum: {df[column_name].min()}")
+        print(f"Maximum: {df[column_name].max()}")
+        
+        # 10-bin histogram
+        plt.hist(df[column_name], bins=num_bins)
+        plt.show()
+
+# get analytics for a given df and column name (for numeric columns)
+def column_analytics_df(df, column_name, num_bins):
+    print(f"Mean: {df[column_name].mean()}")
+    print(f"Median: {df[column_name].median()}")
+    print(f"Standard Deviation: {df[column_name].std()}")
+    print(f"Minimum: {df[column_name].min()}")
+    print(f"Maximum: {df[column_name].max()}")
+    
+    # 10-bin histogram
+    plt.hist(df[column_name], bins=num_bins)
+    plt.show()
+
+# save the list as npy file
+def save_to_npy(list, file_name):
+    np_array = np.array(list)
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, "data", "input", file_name)
+    np.save(file_path, np_array)
+    print(f"Data saved to {file_path}")
+
+# save single pd dataframe to csv
+def save_to_csv(df, file_name):
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, "data", "processed", file_name)
+    df.to_csv(file_path, index=False)
+    print(f"Dataframe saved to {file_path}")
+
+# save multiple pd dataframes to csv
+def save_to_csv_multiple(dfs, file_names):
+    for i in range(len(dfs)):
+        save_to_csv(dfs[i], file_names[i])
+
+# load single pd dataframe from csv
+def load_from_csv(file_name):
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, "data", "processed", file_name)
+    df = pd.read_csv(file_path)
+    return df
+
+
+# load multiple pd daraframes from csv
+def load_from_csv_multiple(file_names):
+    dfs = []
+    for file_name in file_names:
+        dfs.append(load_from_csv(file_name))
+    return dfs
+
+# given a joined patient/event df, split it into dfs for each patient
+def split_by_patients(df):
+    unique_patients = df['SUBJECT_ID'].unique()
+    patient_dfs = []
+    for patient in unique_patients:
+        patient_dfs.append(df[df['SUBJECT_ID'] == patient])
+    return patient_dfs
+
+# given patient-specific event df, aggregate items by hour
+def aggregate_events_by_time(df, intime):
+    # print(intime)
+    # print(pd.to_datetime(intime))
+    intime = pd.to_datetime(intime)
+    df['CHARTTIME'] = pd.to_datetime(df['CHARTTIME'])
+
+    df['HOUR_DIFF'] = ((df['CHARTTIME'] - intime).dt.total_seconds() // 3600).astype(int)
+    hourly_dfs = []
+
+    for hour in range(72):
+        hourly_df = df[df['HOUR_DIFF'] == hour]
+        if hourly_df.empty:
+            # append a empty df
+            hourly_dfs.append(pd.DataFrame())
+            # print(f"Hour {hour}: 0 events")
+        else:
+            hourly_dfs.append(hourly_df)
+            # print(f"Hour {hour}: {len(hourly_df)} events")
+    
+    return hourly_dfs
+
+# given patient-specific event df, aggregate unique items from events df return aggregated dfs for each item
+def aggregate_events_for_item(df, unique_items):
+    item_dfs = []
+    count = 0
+    for item in unique_items:
+        df_item = df[df['ITEMID'] == item]
+        if df_item.empty:
+            # append a empty df
+            item_dfs.append(pd.DataFrame())
+        else:
+            item_dfs.append(df_item)
+            count += 1
+        # print(f"Number of events for item {item}: {df_item.shape[0]}")
+        # print(df_item.head(5))
+    # print(f"Number of unique items: {len(unique_items)}")
+    # print(f"Number of items excluded: {len(unique_items) - len(item_dfs)}")
+    # print(f"Number of items included: {count}\n")
+    return item_dfs
+
+# given patient-time-specific df, calculate the average of "VALUE" column
+def column_average(df, column_name):
+    return df[column_name].mean()
